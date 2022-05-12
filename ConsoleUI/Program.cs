da0b2071ca6f84ed7595cf80e881d0992cabdd3f
Nazmi Becerik <nazmibecerik@icloud.com>
2022-05-12 14:17:38 +0300

    7.Gün Ödevlendirmesi Tamamlandı InMemory Olarak operasyonlar yazıldı ve çalıştırıldı.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager manager = new CarManager(new InMemoryCarDal());
+            foreach (var car in manager.GetAll())
+            {
+                Console.WriteLine(car.Id+" "+car.BrandId+" "+car.ColorId+" "+car.ModelYear+" "+car.DailyPrice+" "+car.Description);
+            }
+            Console.WriteLine("GetById Operasyonu id = 2 ***************************************");
+            var result = manager.GetById(2);
+            Console.WriteLine(result.Id + " " + result.BrandId + " " + result.ColorId + " " + result.ModelYear + " " + result.DailyPrice + " " + result.Description);
+            manager.Add(new Car { Id = 4,BrandId=2,ColorId=3,DailyPrice=400,Description="Mavi mavi",ModelYear=2013 });
+            Console.WriteLine("Add Operasyonu sonrası id 4  ***************************************");
+            foreach (var car in manager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.BrandId + " " + car.ColorId + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+            }
+            Console.WriteLine("Delete Operasyonu**********************************");
+            manager.Delete(new Car {Id=4});
+            foreach (var car in manager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.BrandId + " " + car.ColorId + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+            }
+
+            Console.WriteLine("Update Operasyonu Sonrası**********************************");
+            manager.Update(new Car {Id=2,BrandId=10,ColorId=10,DailyPrice=1000,Description="Güncellendi",ModelYear=2022});
+            foreach (var car in manager.GetAll())
+            {
+                Console.WriteLine(car.Id + " " + car.BrandId + " " + car.ColorId + " " + car.ModelYear + " " + car.DailyPrice + " " + car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+            new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2012,DailyPrice=500,Description="En iyi araba bu araba"},
+            new Car{Id=2,BrandId=2,ColorId=4,ModelYear=2010,DailyPrice=350,Description="En güzel araba bu araba"},
+            new Car{Id=3,BrandId=1,ColorId=2,ModelYear=2017,DailyPrice=800,Description="En hızlı araba bu araba"},
+            };
+
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var deletedToCar = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(deletedToCar);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            var result = _cars.SingleOrDefault(c => c.Id == id);
+            return result;
+        }
+
+        public void Update(Car car)
+        {
+            Car updatedToCar = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            updatedToCar.ColorId = car.ColorId;
+            updatedToCar.BrandId = car.BrandId;
+            updatedToCar.DailyPrice = car.DailyPrice;
+            updatedToCar.Description = car.Description;
+            updatedToCar.ModelYear = car.ModelYear;
+        }
+    }
+}
